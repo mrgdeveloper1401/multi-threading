f1253de0240252b2d9dda5997e4e3cfe91e0a671
mohammad goodarzi <m.goodarzi606@gmail.com>
2023-10-14 10:32:33 +0330

    write deamon

--- deamon.py
+++ deamon.py
@@ -11,8 +11,8 @@ def show(name):
     print(f'Goodbye {name}')
         
         
-t1 = Thread(target=show, args=('mohammad', ))
-t2 = Thread(target=show, args=('ali', ))
+t1 = Thread(target=show, args=('mohammad', ), daemon=True)
+t2 = Thread(target=show, args=('ali', ), daemon=True)
 
 # t1.setDaemon(True)
 # t2.setDaemon(True)
@@ -20,8 +20,8 @@ t2 = Thread(target=show, args=('ali', ))
 t1.start()
 t2.start()
 
-t1.join()
-t2.join()
+# t1.join()
+# t2.join()
 
 # print(t1.isDaemon())
 
